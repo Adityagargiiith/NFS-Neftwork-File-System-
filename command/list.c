7ad8e51b76ab17c043911fe8d64a39f526e19c08
Aditya Garg <adityagarg@pop-os.localdomain>
2023-11-21 18:53:13 +0530

    fix: list

--- command/list.c
+++ command/list.c
@@ -5,11 +5,14 @@ void list(char *input)
         char *array[2];
  array[0] = strtok(input, " ");
         array[1] = strtok(NULL, " ");
+        // array[2] = strtok(NULL, " ");
  DIR *dir;
     struct dirent *entry;
     char path[50];
     // char dot[10];
     strcpy(path, ".");
+    // strcat(path, array[2]);
+    // strcat(path, "/");
     strcat(path, array[1]);
     dir = opendir(path);
 
@@ -21,7 +24,6 @@ void list(char *input)
     while ((entry = readdir(dir)) != NULL) {
         printf("%s\n", entry->d_name);
     }
-    
     closedir(dir);
 
 }
